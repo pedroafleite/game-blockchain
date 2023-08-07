55ae0a191282eb60aebf60590417657c87a718d7
pedroafleite <pedroaug.fl@gmail.com>
2023-08-06 22:28:34 -0300

    config thirdweb

--- Assets/Scripts/SDKManager.cs
+++ Assets/Scripts/SDKManager.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Thirdweb;
 
 public class SDKManager : MonoBehaviour
 {
+    public static SDKManager Instance;
+
+    public ThirdwebSDK SDK;
+
+    private void Awake() {
+        if (Instance != null) {
+            Destroy(gameObject);
+        } else {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        SDK = new ThirdwebSDK("goerli");
         
     }
 
---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
+
+public class StartManager : MonoBehaviour
+{
+    public GameObject connected;
+    public GameObject disconnected;
+    public GameObject enterBtn;
+    public TMPro.TextMeshProUGUI addressTxt;
+    public TMPro.TextMeshProUGUI ownsNFTTxt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
